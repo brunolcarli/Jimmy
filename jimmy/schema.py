2371eba44da47b5e0eb52f382131243ec728cfaf
Bruno L. Carli <brunolcarli@gmail.com>
2023-05-10 04:34:40 -0300

    ADD create answer mutation

--- jimmy/schema.py
+++ jimmy/schema.py
@@ -1,10 +1,12 @@
 import graphene
-
 import kinnon.schema
 
 
 class Query(kinnon.schema.Query, graphene.ObjectType):
     pass
 
+class Mutation(kinnon.schema.Mutation, graphene.ObjectType):
+    pass
 
-schema = graphene.Schema(query=Query)
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- kinnon/schema.py
+++ kinnon/schema.py
@@ -4,8 +4,13 @@ from django.conf import settings
 
 
 class QuestionType(graphene.ObjectType):
+    id = graphene.ID()
     question = graphene.String()
     scope = graphene.String()
+    answers = graphene.List('kinnon.schema.AnswerType')
+
+    def resolve_answers(self, info, **kwargs):
+        return self.answer_set.all()
 
 
 class AnswerType(graphene.ObjectType):
@@ -30,3 +35,43 @@ class Query:
     answers = graphene.List(AnswerType)
     def resolve_answers(self, info, **kwargs):
         return Answer.objects.filter(**kwargs)
+
+
+class CreateAnswer(graphene.relay.ClientIDMutation):
+    answer = graphene.Field(AnswerType)
+
+    class Input:
+        user_id = graphene.String()
+        username = graphene.String()
+        text_answer = graphene.String(required=True)
+        question_id = graphene.ID(required=True)
+        sentiment = graphene.Boolean()
+
+    def mutate_and_get_payload(self, info, **kwargs):
+        username = kwargs.get('username')
+        user_id = kwargs.get('user_id')
+        if (not username) and (not user_id):
+            is_anonymous = True
+        else:
+            is_anonymous = False
+
+        try:
+            question = Question.objects.get(id=kwargs.get('question_id'))
+        except Question.DoesNotExist:
+            raise Exception('Invalid or not found question!')
+
+        answer = Answer.objects.create(
+            user_id=user_id,
+            username=username,
+            is_anonymous=is_anonymous,
+            text_answer=kwargs['text_answer'],
+            question=question,
+            sentiment=kwargs.get('sentiment')
+        )
+        answer.save()
+
+        return CreateAnswer(answer)
+
+
+class Mutation:
+    create_answer = CreateAnswer.Field()
