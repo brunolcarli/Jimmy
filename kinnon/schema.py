be077d607826cb8ab2591b54676ab5e44ff0f225
Bruno L. Carli <brunolcarli@gmail.com>
2023-05-10 04:07:05 -0300

    ADD answer and question API aqueries

--- kinnon/schema.py
+++ kinnon/schema.py
@@ -1,5 +1,21 @@
 import graphene
+from kinnon.models import Question, Answer
 from django.conf import settings
+
+
+class QuestionType(graphene.ObjectType):
+    question = graphene.String()
+    scope = graphene.String()
+
+
+class AnswerType(graphene.ObjectType):
+    user_id = graphene.String()
+    username = graphene.String()
+    is_anonymous = graphene.Boolean()
+    text_answer = graphene.String()
+    question = graphene.Field(QuestionType)
+    datetime = graphene.DateTime()
+    sentiment = graphene.Boolean()
 
 
 class Query:
@@ -7,3 +23,10 @@ class Query:
     def resolve_version(self, info, **kwargs):
         return settings.VERSION
 
+    questions = graphene.List(QuestionType)
+    def resolve_questions(self, info, **kwargs):
+        return Question.objects.filter(**kwargs)
+
+    answers = graphene.List(AnswerType)
+    def resolve_answers(self, info, **kwargs):
+        return Answer.objects.filter(**kwargs)
