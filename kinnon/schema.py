71c6516c1e2723c3afd5ae432711211e8d902168
Bruno L. Carli <brunolcarli@gmail.com>
2023-05-13 17:45:02 -0300

    FIX less and most sorting retrieve

--- kinnon/schema.py
+++ kinnon/schema.py
@@ -61,12 +61,12 @@ class Query:
     less_answered_question  = graphene.Field(QuestionType)
 
     def resolve_less_answered_question(self, info, **kwargs):
-        return Question.objects.order_by().annotate(Count('answer')).last()
+        return Question.objects.order_by('answer').annotate(Count('answer')).first()
 
     most_answered_question  = graphene.Field(QuestionType)
 
     def resolve_most_answered_question(self, info, **kwargs):
-        return Question.objects.order_by().annotate(Count('answer')).first()
+        return Question.objects.order_by('answer').annotate(Count('answer')).last()
 
 
 class CreateAnswer(graphene.relay.ClientIDMutation):
